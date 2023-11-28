94c9643c94059ca0bfeff1287f6a3ad099c7aec7
Roshankumar S <2018che0337@svce.ac.in>
2023-11-28 16:24:34 +0530

    Files modified

--- Predictions.py
+++ Predictions.py
@@ -1,16 +1,18 @@
 from statsmodels.tsa.arima.model import ARIMA
 import numpy as np
-import MetaTrader5 as mt5
 import pandas as pd
 import datetime
 import streamlit as st
+import requests
+from sklearn.preprocessing import MinMaxScaler
+import pmdarima as pm
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+# from prophet import Prophet
 
-# Initialize MT5 connection
-if not mt5.initialize():
-    print("initialize() failed, error code =",mt5.last_error())
-    quit()
 
-def get_predictions(currency_pair,timeframe):
+def get_predictions_ARIMA(currency_pair,timeframe,fine_tuning_method,API_KEY):
 
     time_freq_dict = {
     "1 Min": "T",
@@ -23,28 +25,22 @@ def get_predictions(currency_pair,timeframe):
     "1 Week": "W",
     "1 Month": "M"}
 
-    timeframes = {"1 Min":mt5.TIMEFRAME_M1,"5 Min":mt5.TIMEFRAME_M5,
-                  "15 Min":mt5.TIMEFRAME_M15,"30 Min":mt5.TIMEFRAME_M30,
-                  "1 Hour":mt5.TIMEFRAME_H1,"4 Hour":mt5.TIMEFRAME_H4,
-                  "6 Hour":mt5.TIMEFRAME_H6,"1 Day":mt5.TIMEFRAME_D1,
-                  "1 Week":mt5.TIMEFRAME_W1,"1 Month":mt5.TIMEFRAME_MN1}
+    df = get_data(currency_pair,timeframe,API_KEY)
 
-    # Get historical data
-    rates = mt5.copy_rates_from_pos(currency_pair, timeframes[timeframe], 0, 1000)
-
-    df = pd.DataFrame(rates)
-
-    df.columns = ["Timestamp","Open","High","Low","Close","Tick Volume","Spread","Real Volume"]
-    df.index = df.Timestamp.apply(lambda a : datetime.datetime.fromtimestamp(a))
+    if fine_tuning_method=="PmdArima":
+        # Fit a simple auto_arima model
+        model = pm.auto_arima(df["Close"].astype(float), seasonal=True, m=12)
+        order = model.order
+    else:
+        order=(1,3,1)
      
     # Use 'close' column for ARIMA model
-    model = ARIMA(df["Close"], order=(1,3,1))
+    model = ARIMA(df["Close"].astype(float), order=order)
 
     # Fit the model
     model_fit = model.fit()
 
     past = model_fit.predict()
-
 
     # Forecast
     forecast = model_fit.forecast(steps=30)
@@ -65,22 +61,128 @@ def get_predictions(currency_pair,timeframe):
 
     return df_forecast
 
-def get_data(currency_pair,timeframe):
+# def get_predictions_prophet(currency_pair,timeframe,API_KEY):
+
+#     df = get_data(currency_pair,timeframe,API_KEY)
+#     df["Timeframe"]=df.index
+#     df = df.rename(columns={'Timeframe': 'ds', 'Close': 'y'})
+#     df['ds'] = df['ds'].dt.tz_localize(None) 
+
+#     # Create a Prophet model
+#     model = Prophet()
+#     model.fit(df.astype(float))
+
+#     # Specify the number of periods to forecast into the future
+#     n_periods = 30  # Replace with the number of periods you want to forecast
     
-    timeframes = {"1 Min":mt5.TIMEFRAME_M1,"5 Min":mt5.TIMEFRAME_M5,
-                  "15 Min":mt5.TIMEFRAME_M15,"30 Min":mt5.TIMEFRAME_M30,
-                  "1 Hour":mt5.TIMEFRAME_H1,"4 Hour":mt5.TIMEFRAME_H4,
-                  "6 Hour":mt5.TIMEFRAME_H6,"1 Day":mt5.TIMEFRAME_D1,
-                  "1 Week":mt5.TIMEFRAME_W1,"1 Month":mt5.TIMEFRAME_MN1}
+#     past = model.predict()
+#     # Create a DataFrame with future dates
+#     future = model.make_future_dataframe(periods=n_periods)
 
-    # Get historical data
-    rates = mt5.copy_rates_from_pos(currency_pair, timeframes[timeframe], 0, 1000)
+#     # Use the fitted model to make forecasts
+#     forecast = model.predict(future)
+    
+#     # Extract the forecasted values and relevant columns
+#     forecast_df = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
-    df = pd.DataFrame(rates)
+#     return forecast_df["yhat"]
 
-    df.columns = ["Timestamp","Open","High","Low","Close","Tick Volume","Spread","Real Volume"]
+# def get_predictions_LSTM(currency_pair,timeframe,API_KEY):
 
-    df.Timestamp = df.Timestamp.apply(lambda a : datetime.datetime.fromtimestamp(a))
+#     time_freq_dict = {
+#     "1 Min": "T",
+#     "5 Min": "5T",
+#     "15 Min": "15T",
+#     "30 Min": "30T",
+#     "1 Hour": "H",
+#     "4 Hour": "4H",
+#     "1 Day": "D",
+#     "1 Week": "W",
+#     "1 Month": "M"}
+
+#     df = get_data(currency_pair,timeframe,API_KEY)
+
+#     # Initialize Min-Max Scaler
+#     scaler = MinMaxScaler()
+
+#     target = pd.DataFrame(data=df["Close"],columns=["Close"])
+
+#     # Scale the target variable using the scaler
+#     target["Scaled Data"] = scaler.fit_transform(target[["Close"]])
+
+#     X=[]
+#     y=[]
+#     # Extract 100 records as independent values and 101st record as target value over each iteration
+#     for i in range(len(target)-100):
+#         X.append(list(target.iloc[i:i+100,1]))
+#         y.append(target.iloc[i+100,1])
+
+#     # Initialize a sequential model using LSTM algorithm
+#     model=Sequential()
+#     model.add(LSTM(50,return_sequences=True,input_shape=(100,1)))
+#     model.add(LSTM(50,return_sequences=True))
+#     model.add(LSTM(50))
+#     model.add(Dense(1))
+#     model.compile(loss='mean_squared_error',optimizer='adam')
+
+#     # Train model using train and test data
+#     model.fit(X,y,validation_data=(X,y),epochs=100,batch_size=64,verbose=1)
+
+#     # Store input values of prediction
+#     predict_input = target.iloc[-100:,1].values.tolist()
+#     # Create empty list to store output values of prediction
+#     predict_output = []
+#     # Create list to store both input and output data
+#     predict_data = predict_input.copy()
+
+#     # Iterate for loop to predict prices of 30 successive days
+#     for i in range(30):
+#         # Pass 100 values as input
+#         step_input = predict_data[-100:]
+#         # Predict 101st value using developed model
+#         step_output = model.predict([list(step_input)],verbose=0).reshape(1)
+#         # Add the predicted value to the input value
+#         predict_data.extend(step_output)
+#         # Store the output values in a list
+#         predict_output.extend(step_output)
+
+#     # Transform output data
+#     predicted = scaler.inverse_transform(np.array(predict_output).reshape(-1,1)).reshape(-1)
+#     forecasted = np.concatenate([df["Close"].values,predicted])
+#     difference = len(forecasted)-len(df)
+#     fill = [np.nan for i in range(difference)]
+#     actual = np.concatenate([df["Close"].values,fill])
+
+#     df_forecast = pd.DataFrame({"Predicted":forecasted,"Actual":actual})
+#     new_idx = pd.date_range(df.index[-1], periods=31, freq=time_freq_dict[timeframe])[1:]
+#     idx = np.concatenate([df.index,new_idx])
+#     df_forecast.index = idx
+#     st.dataframe(df_forecast)
+#     return df_forecast
+
+def get_data(currency_pair,timeframe,API_KEY):
+    
+    # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    timeframes = {"1 Min":"function=TIME_SERIES_INTRADAY&interval=1min",
+                  "5 Min":"function=TIME_SERIES_INTRADAY&interval=5min",
+                  "15 Min":"function=TIME_SERIES_INTRADAY&interval=15min",
+                  "30 Min":"function=TIME_SERIES_INTRADAY&interval=30min",
+                  "1 Hour":"function=TIME_SERIES_INTRADAY&interval=60min",
+                  "1 Day":"function=TIME_SERIES_DAILY",
+                  "1 Week":"function=TIME_SERIES_WEEKLY",}
+    timeseries = {"1 Week":"Weekly Time Series","1 Day":"Time Series (Daily)",
+                  "1 Hour":"Time Series (60min)","30 Min":"Time Series (30min)",
+                  "15 Min":"Time Series (15min)","5 Min":"Time Series (5min)",
+                  "1 Min":"Time Series (1min)"}
+    url = "https://www.alphavantage.co/query?"+timeframes[timeframe]+"&symbol="+currency_pair+"&apikey="+API_KEY+"&outputsize=full"
+    r = requests.get(url)
+    data = r.json()
+
+    df = pd.DataFrame(data[timeseries[timeframe]]).T[::-1][:1000]
+    df.columns = ["Open","High","Low","Close","Volume"]
+
+    for col in df.columns:
+        df[col] = df[col].astype(float)
 
     return df
 
--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Predictions import get_predictions,get_data
+from Predictions import get_predictions_ARIMA,get_predictions_LSTM,get_predictions_prophet,get_data
 import pandas as pd
 import numpy as np
 import pandas_profiling
@@ -19,14 +19,29 @@ _,form,_ = st.columns([1,3,1])
 with form:
     st.title("Forex Forecasting")
     # Generate quiz
+    st.write("""
+    Ever looked a forex chart and wondered how well you understood its trend? Here's a fun twist: Instead of just looking at chart, come to **Forex Forecasting** and predict the prices!
+
+    **How does it work?** 🤔
+    1. Select the currency pair.
+    2. Select the timeframe in which you want to trade.
+    3. Select the model which you want to use to forecast the market.         
+    4. Enter your [Alpha Advantage API Key].
+       Claim your free alpha advantage API key from here.(https://www.alphavantage.co/support/#api-key).
+
+    ⚠️ Disclaimer: These predictions are purely for educational purpose.
+
+    Once you've input the details, voilà! Dive deep into predictions and analysis of the market! 
+    """)
     with st.form("user_input"):
         currency_pairs = ["EURUSD","GBPUSD","USDCHF","USDJPY","USDCNH","USDRUB","AUDUSD","NZDUSD","USDCAD","USDSEK"]
         currency_pair = st.selectbox("Which currency pair would you like to forecast ?",currency_pairs)
-        time_frames = ["1 Min","5 Min","15 Min","30 Min","1 Hour","4 Hour","1 Day","1 Week","1 Month"]
+        time_frames = ["1 Min","5 Min","15 Min","30 Min","1 Hour","1 Day","1 Week"]
         time_frame = st.selectbox("Select the timeframe for forecasting",time_frames,index=4)
         method = st.radio("Select Forecasting method:",["ARIMA","Prophet","LSTM"])
         fine_tuning_methods = ["None","PmdArima","GridSearchCV"]
         fine_tuning = st.radio("Which fine tuning method you would like to use for ARIMA?",fine_tuning_methods,horizontal=True,index=0)
+        API_KEY = st.text_input("Enter your OpenAI API Key:", placeholder="sk-XXXX", type='password')
         submitted = st.form_submit_button("Forecast!")
 
 
@@ -36,11 +51,16 @@ if submitted:
 
     with profile:
 
-        st.title("Forex data (1000 records)")
-        data = get_data(currency_pair,time_frame)
+        data = get_data(currency_pair,time_frame,API_KEY)
+        st.title(currency_pair+" data ("+str(len(data))+" records)")
         st.dataframe(data)
 
-    df = get_predictions(currency_pair,time_frame)
+    if method=="ARIMA":
+        df = get_predictions_ARIMA(currency_pair,time_frame,fine_tuning,API_KEY)
+    elif method=="LSTM":
+        df = get_predictions_LSTM(currency_pair,time_frame,API_KEY)
+    else: 
+        df = get_predictions_ARIMA(currency_pair,time_frame,API_KEY)
 
     _,chart,_ = st.columns([1,3,2])
 
@@ -48,13 +68,18 @@ if submitted:
         
         st.title("Forecasting Results")
         st.subheader("Actual Price")
+
+        df["Predicted"]=df["Predicted"].astype(float)
+        df["Actual"]=df["Actual"].astype(float)
+
         # Create a Seaborn pairplot
-        plot = sns.lineplot(x=df.index,y=df["Actual"],)
+        plot = sns.lineplot(x=np.arange(len(df)),y=df["Actual"].astype(float))
         # Display the plot in Streamlit
         st.pyplot(plot.get_figure())
         plt.show()
 
         st.subheader("Forecasting results")
+        
         st.line_chart(df)
 
 
